f5a0eff17f5264337d4368faada1d3afc1b8f9be
unknown <saddam.bay20@gmail.com>
2022-04-23 10:26:00 +0545

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,24 +10,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("National Infotech"),
-          centerTitle: true,
-          backgroundColor: Colors.red,
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Text(
-                "Programs Offered",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("National Infotech"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Programs Offered",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              Padding(
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => PlusTwo()));
+              },
+              child: Padding(
                 padding: const EdgeInsets.only(top: 20),
                 child: Container(
                   alignment: Alignment.center,
@@ -42,24 +61,108 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 80,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "Bsc. CSIT",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Container(
+                alignment: Alignment.center,
+                height: 80,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(20)),
+                child: Text(
+                  "Bsc. CSIT",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Container(
+                alignment: Alignment.center,
+                height: 80,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(20)),
+                child: Text(
+                  "BCA",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            )
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class PlusTwo extends StatelessWidget {
+  PlusTwo({Key? key}) : super(key: key);
+
+  final List plusTwoStudents = [
+    {
+      "name": "Saddam Husen",
+      "image":
+          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+    },
+    {
+      "name": "Mustafa Hawari",
+      "image":
+          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+    },
+    {
+      "name": "Anish Husen",
+      "image":
+          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+    },
+    {
+      "name": "Bikash Husen",
+      "image":
+          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("+2"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView.builder(
+            itemCount: plusTwoStudents.length,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Row(
+                    children: [
+                      Container(
+                          width: 70,
+                          height: 70,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  plusTwoStudents[index]["image"],
+                                ),
+
+                              fit: BoxFit.cover
+                              ),
+                              shape: BoxShape.circle,
+                              color: Colors.grey)),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          plusTwoStudents[index]["name"],
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+            ),),
       ),
     );
   }
